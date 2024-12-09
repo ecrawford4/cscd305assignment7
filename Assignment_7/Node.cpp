744015c74a93a732aff293d623336af0a9f7c5a5
Ethan Crawford <ethan.crawford5532@gmail.com>
2024-12-09 20:26:37 +0000

    rewrite with smart pointers

--- Assignment_7/Node.cpp
+++ Assignment_7/Node.cpp
@@ -1,66 +1,42 @@
+/**
+ * Node.cpp
+ * 
+ * edited by Ethan Crawford
+ * 
+ * g++ (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0
+ * Copyright (C) 2019 Free Software Foundation, Inc.
+ */
 #include "Node.h"
-
-template <class T>
-T Node<T>::getValue()
-{
-	return Value;
-}
-
-template <class T>
-
-Node<T>* Node<T>::getNext()
-{
-	return next;
-}
-
-template <class T>
-void Node<T>::setNext(Node<T>* nextPtr)
-{
-	next = nextPtr;
-}
-
-template <class T>
-Node<T>* Node<T>::getPrevious()
-{
-	return prev;
-}
-
-template <class T>
-void Node<T>::setPrevious(Node<T>* prevPtr)
-{
-	prev = prevPtr;
-}
-
-
-template<typename T>
-void PrintForward(Node<T>* start) {
-	Node<T>* temp = start;
-
-	while (temp) {
-
-		cout << temp->getValue() << "-> ";
-		temp = temp->getNext();
-	}
-	cout << endl;
-}
-template<typename T>
-void PrintReverse(Node<T>* end) {
-
-	Node<T>* temp = end;
-	while (temp) {
-		cout << "<-" << temp->getValue() << " ";
-		temp = temp->getPrevious();
-	}
-	cout << endl;
-}
-
+/**
+ * PrintForward method to print the doubly linked list
+ */
 template <typename T>
-void deleteList(Node<T>* start)
-{
-	Node<T>* temp = start;
-	while (start) {
-		temp = start;
-		start = start->getNext();
-		delete(temp);
-	}
+void PrintForward(shared_ptr<Node<T>> start) {
+    auto temp = start;
+    while (temp) {
+        cout << temp->getValue() << " -> ";
+        temp = temp->getNext();
+    }
+    //cout << "nullptr" << endl;
+	cout << endl;
 }
+/**
+ * PrintReverse method to print the doubly linked list in reverse order
+ */
+template <typename T>
+void PrintReverse(shared_ptr<Node<T>> end) {
+    auto temp = end;
+    //cout << "nullptr ";
+    while (temp) {
+        cout << "<- " << temp->getValue() << " ";
+        auto prev = temp->getPrevious().lock(); // Use lock() to access the shared_ptr
+        temp = prev;
+    }
+	cout << endl;
+}
+
+/**
+ * Explicit instantiation for common types (if required)
+ */
+template void PrintForward<string>(shared_ptr<Node<string>> start);
+template void PrintReverse<string>(shared_ptr<Node<string>> end);
--- Assignment_7/Node.h
+++ Assignment_7/Node.h
@@ -1,28 +1,49 @@
+/**
+ * Node.h
+ * 
+ * edited by Ethan Crawford
+ * 
+ * g++ (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0
+ * Copyright (C) 2019 Free Software Foundation, Inc.
+ */
 #ifndef NODE_H
 #define NODE_H
-
 #include <iostream>
+#include <memory>
 #include <string>
-
 using namespace std;
-
+/**
+ * Node 
+ */
 template <typename T>
 class Node {
 private:
-	T Value;
-	Node<T>* prev;
-	Node<T>* next;
+    T value;
+    weak_ptr<Node<T>> prev;
+    shared_ptr<Node<T>> next;
+
 public:
-	Node<T>(T val) : Value(val) {}
-	~Node<T>() {
-		cout << "Deleting node with a value " << Value << endl;
-	}
-	T getValue();
-	void setValue(T val) { Value = val; }
-	Node<T>* getNext();
-	Node<T>* getPrevious();
-	void setNext(Node<T>* nextPtr);
-	void setPrevious(Node<T>* previousPtr);
+    Node(T val) : value(val) {}
+    ~Node() {
+        cout << "Deleting node with value " << value << endl;
+    }
+
+    T getValue() const { return value; }
+    void setValue(T val) { value = val; }
+
+    shared_ptr<Node<T>> getNext() const { return next; }
+    weak_ptr<Node<T>> getPrevious() const { return prev; }
+
+    void setNext(shared_ptr<Node<T>> nextPtr) { next = nextPtr; }
+    void setPrevious(shared_ptr<Node<T>> previousPtr) { prev = previousPtr; }
 };
+/**
+ * Explicit instantiation for common types (if required)
+ */
+template <typename T>
+void PrintForward(shared_ptr<Node<T>> start);
+
+template <typename T>
+void PrintReverse(shared_ptr<Node<T>> end);
 
 #endif
